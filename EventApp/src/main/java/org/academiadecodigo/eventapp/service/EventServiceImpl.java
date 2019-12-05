f709157ca3735e35df6010ba17e465291f856e84
Vonderr <vando.mfc@gmail.com>
2019-12-05 15:30:54 +0000

    eventService implementation

--- EventApp/src/main/java/org/academiadecodigo/eventapp/service/EventServiceImpl.java
+++ EventApp/src/main/java/org/academiadecodigo/eventapp/service/EventServiceImpl.java
@@ -1,27 +1,40 @@
 package org.academiadecodigo.eventapp.service;
 
+import org.academiadecodigo.eventapp.dao.EventDao;
 import org.academiadecodigo.eventapp.model.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public class EventServiceImpl implements EventService {
+
+    private EventDao eventDao;
+
+    @Autowired
+    public void setEventDao(EventDao eventDao) {
+        this.eventDao = eventDao;
+    }
+
     @Override
     public Event get(Integer id) {
-        return null;
+        return eventDao.findById(id);
     }
 
     @Override
     public List<Event> findAll() {
-        return null;
+        return eventDao.findAll();
     }
 
     @Override
+    @Transactional
     public void save(Event event) {
-
+        eventDao.saveOrUpdate(event);
     }
 
     @Override
+    @Transactional
     public void delete(Integer id) {
-
+        
     }
 }
